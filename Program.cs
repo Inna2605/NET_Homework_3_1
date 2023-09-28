f7df93b60ab5865cbd45eecf2449e6f1c9074c6f
Inna <prizrak1609@gmail.com>
2023-09-28 15:52:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Створіть клас «Веб-сайт». Необхідно зберігати у полях класу: назву сайту, шлях до сайту, опис сайту, ip-адресу сайту.
+//Реалізуйте методи класу для введення даних, виведення даних. Реалізуйте доступ до окремих полів через методи класу. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NET_Homework_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Website MySite = new Website("My Site", "http://www.mysite.com", "Це мій власний веб-сайт");
+
+            Console.WriteLine("Початкові дані сайту:");
+            MySite.OutputData();
+            Console.WriteLine();
+
+            MySite.InputData("Нова назва", "http://www.newsite.com", "Опис нового сайту");
+            Console.WriteLine("Змінені дані сайту:") ;
+            MySite.OutputData();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Website.cs
@@ -0,0 +1,54 @@
+﻿//Створіть клас «Веб-сайт». Необхідно зберігати у полях класу: назву сайту, шлях до сайту, опис сайту, ip-адресу сайту.
+//Реалізуйте методи класу для введення даних, виведення даних. Реалізуйте доступ до окремих полів через методи класу. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Homework_3_1
+{
+    internal class Website
+    {
+        private string _url;
+        private string _title;
+        private string _description;
+
+        public Website(string name, string url, string description)
+        {
+            _title = name;
+            _url = url;
+            _description = description;
+        }
+
+        public void InputData(string title, string url, string description)
+        {
+            _title = title;
+            _url = url;
+            _description = description;
+        }
+
+        public void OutputData()
+        {
+            Console.WriteLine($"Назва сайту: {_title}");
+            Console.WriteLine($"Шлях до сайту: {_url}");
+            Console.WriteLine($"Опис сайту: {_description}");
+        }
+        public string SiteName 
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string SiteUrl 
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+        public string SiteDescription 
+        {
+            get { return _description; }
+            set { _description = value; }
+        }         
+    }
+}
